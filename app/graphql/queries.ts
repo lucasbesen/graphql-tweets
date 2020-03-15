858ffc70a206be30d9cf944fc08ce197475dddbb
lucasbesen <lucassilvabesen@gmail.com>
2020-03-15 00:07:27 -0300

    feat(tweets): create graphql query for tweets

---
+++ app/graphql/queries.ts
@@ -0,0 +1,37 @@
+import gql from 'graphql-tag';
+
+export interface IUser {
+  id: string;
+  screen_name: string;
+  name: string;
+  profile_image_url: string;
+}
+
+export interface ITweet {
+  id: string;
+  created_at: string;
+  text: string;
+  retweet_count: number;
+  user: IUser;
+}
+
+export enum ResultType {
+  Mixed = 'mixed',
+  Recent = 'recent',
+  Popular = 'popular',
+}
+
+export const GET_TWEETS = gql`
+  query getTweets($q: String!, $count: Int!, $resultType: SearchReponse!) {
+    twitter {
+      search(q: $q, count: $count, result_type: $resultType) {
+        id
+        text
+        created_at
+        user {
+          name
+        }
+      }
+    }
+  }
+`;
