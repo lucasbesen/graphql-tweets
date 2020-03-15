57f5bdc9653e7b4f4bcb9331ae54aba7cb9b8095
lucasbesen <lucassilvabesen@gmail.com>
2020-03-15 17:00:55 -0300

    feat(tweets): use typescript on home screen and handle with loading

--- app/components/screens/HomeScreen/HomeScreen.tsx
+++ app/components/screens/HomeScreen/HomeScreen.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import { ActivityIndicator } from 'react-native';
+import { RouteProp } from '@react-navigation/stack';
 
-import { Container, Text } from 'app/components/common';
+import { useTweetsQuery } from 'app/hooks';
+import { Container, Text, Wrapper } from 'app/components/common';
+import { StackParamsList } from 'app/config/routes';
 
-const HomeScreen = () => {
+type HomeScreenRouteProp = RouteProp<StackParamsList, 'Home'>;
+
+type Props = {
+  route: HomeScreenRouteProp;
+};
+
+const HomeScreen = ({ route: { params } }: Props) => {
+  const search = params?.search;
+  const [isLoading] = useTweetsQuery(search);
+
+  if (isLoading) {
+    return (
+      <Container>
+        <Wrapper flex={1} centralize>
+          <ActivityIndicator />
+        </Wrapper>
+      </Container>
+    );
+  }
+
   return (
     <Container>
       <Text>That's the Home screen </Text>
