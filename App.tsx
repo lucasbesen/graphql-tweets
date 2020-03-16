afbbcec14b78047250974dfd5844ca407a3e7106
lucasbesen <lucassilvabesen@gmail.com>
2020-03-15 21:19:13 -0300

    feat(app): add profile picture in the header

--- App.tsx
+++ App.tsx
@@ -10,13 +10,18 @@ import { ApolloProvider } from '@apollo/react-hooks';
 import apolloClient from 'app/graphql/apolloClient';
 import ROUTES, { StackParamsList } from 'app/config/routes';
 import { HomeScreen } from 'app/components/screens';
+import { ProfilePicture } from 'app/components/common';
 import theme from 'app/config/theme';
 
 const Stack = createStackNavigator<StackParamsList>();
 
 const stackOptions: StackNavigationOptions = {
   headerTransparent: true,
+  headerLeft: () => (
+    <ProfilePicture size={45} mt={20} ml={10} borderRadius={25} />
+  ),
   headerTintColor: theme.colors.text.white,
+  headerTitleStyle: { marginTop: 20, fontSize: 20 },
 };
 
 const MainStack = () => (
